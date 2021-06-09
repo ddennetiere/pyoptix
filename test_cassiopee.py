095f8431a5529e974f2451062826850db8b0338b
ddennetiere <david.dennetiere@gmail.com>
2021-06-09 18:58:42 +0200

    feat: modification of tests

--- test_cassiopee.py
+++ test_cassiopee.py
@@ -1,11 +1,25 @@
 # coding: utf-8
 import numpy as np
 from lxml import etree
+from classes import OpticalElement, Beamline
 
 tree = etree.parse(r"D:\Dennetiere\optix\bin\test\system.xml")
+beamline = Beamline()
 for user in tree.xpath("/system/element"):
-    print(user.get("name"))
-    print(user.get("class"))
-    print(user.get("next"))
+    new_element = OpticalElement(name=user.get("name"), next=user.get("next"), previous=user.get("previous"))
+    beamline.add_element(new_element)
     for param in user.xpath("/"):
         print(param.text)
+for element in beamline.elements:
+    print(element)
+print("#"*80)
+beamline.chain()
+
+for element in beamline.elements:
+    print(element)
+print("#"*80)
+for chain in beamline.chains:
+    desc = ""
+    for element in chain:
+        desc += element.name+" -> "
+    print(desc)
