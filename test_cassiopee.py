bb6ac429cdf8439bae0c1abbffbbd32fd7fcee6d
David DENNETIERE <david.dennetiere@synchrotron-soleil.fr>
2021-07-23 18:41:03 +0200

    feat: Multiple change for tests
    direct call to exposed functions in this file will be removed, at the
    end only calls to OE and beamline objects and methods should remain

--- test_cassiopee.py
+++ test_cassiopee.py
@@ -8,14 +8,14 @@ from ctypes import WINFUNCTYPE, Structure, pointer, byref, POINTER, c_char_p, c_
 import numpy as np
 from classes import Beamline, OpticalElement, Parameter, Diagram, RecordingMode
 from exposed_functions import *
-from ui_objects import scatter_plot_2d, show
+from ui_objects import scatter_plot_2d, show, plot_spd
 
 global optix
 
 if __name__ == "__main__":
     test_ID = False
     test_enumerate = False
-    test_parameter = False
+    test_parameter = True
     test_edit_parameter = False
     test_linkage = True
     test_add_element = True
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     # initialisation auto
     try:
         test = optix
-        print(test, "  already initialized")
+        print(test, "already initialized")
     except NameError:
         optix = load_optix()
         print("OptiX library initialized")
@@ -33,18 +33,17 @@ if __name__ == "__main__":
     load_solemio_file(create_string_buffer(b"D:\\Dennetiere\\Programmes Python\\optix\\solemio\\CASSIOPEE"))
     hsys, hparam, elemID = HANDLE(0), HANDLE(0), HANDLE(0)
     elname = create_string_buffer(32, c_char)
-    if test_parameter:
+    if test_parameter or test_edit_parameter:
         param_name = create_string_buffer(48)
         param = Parameter()
     print("#" * 80)
     enumerate_elements(hsys, elemID, elname)
     sourceID = None
     elements_ID = {}
-    if test_add_element:
-        elements_ID["screen"] = create_element("PlaneFilm", "screen")
-    start = 1
-    while hsys or start:
-        start = 0
+    start = True
+    last = False
+    while hsys or start or last:
+        start = False
         print("-" * 20)
         print("hsys", hsys)
         if test_enumerate or test_parameter:
@@ -58,7 +57,12 @@ if __name__ == "__main__":
                 enumerate_parameters(elemID, hparam, param_name, param, confirm=False)
             print("\t", f"{param_name.value.decode()}: {param.value} [{param.bounds.min}, {param.bounds.max}],"
                         f"x{param.multiplier}, type {param.type}, groupe {param.group}, flags {param.flags}")
-        enumerate_elements(hsys, elemID, elname, confirm=test_enumerate)
+        if not last:
+            enumerate_elements(hsys, elemID, elname, confirm=test_enumerate)
+            if not hsys:
+                last = True
+        else:
+            last = False
 
     if test_ID:
         print(elements_ID)
@@ -75,7 +79,7 @@ if __name__ == "__main__":
 
     if test_edit_parameter:
         nrays = Parameter()
-        enumerate_parameters(sourceID, hparam, param_name, param, confirm=False)
+        enumerate_parameters(sourceID, hparam, param_name, param, confirm=True)
         while hparam:
             print("\t", param_name.value, param.value, param.bounds.min, param.bounds.max, param.multiplier, param.type,
                   param.group, param.flags)
@@ -83,7 +87,7 @@ if __name__ == "__main__":
 
         get_parameter(sourceID, "nRays", nrays)
         print("Imported nrays", nrays.value)
-        nrays.value = 10000
+        nrays.value = 50000
         set_parameter(sourceID, "nRays", nrays)
         nrays2 = Parameter()
         get_parameter(sourceID, "nRays", nrays2)
@@ -96,24 +100,23 @@ if __name__ == "__main__":
         next_ID = sourceID
         this_ID = HANDLE(sourceID.value)
         while next_ID:
-            get_element_name(this_ID, elname, show_return=True)
+            get_element_name(this_ID, elname, show_return=False, confirm=False)
             linked_beamline.append(elname.value.decode())
-            # find_next_element(this_ID, next_ID, show_return=True)
-            next_ID = get_next_element(this_ID, show_return=True)
+            next_ID = get_next_element(this_ID, show_return=False, confirm=False)
             this_ID = next_ID
         print("Chained beamline :", linked_beamline)
         for oe_name in linked_beamline:
-            oe = OpticalElement()
-            oe.from_element_id(elements_ID[oe_name])
+            oe = OpticalElement(element_id=elements_ID[oe_name])
             print(oe)
 
     if test_add_element:
-        # elements_ID["screen"] = create_element("PlaneFilm", "screen")
+        elements_ID["screen"] = create_element("PlaneFilm", "screen")
         print("screen_id", elements_ID["screen"])
         get_element_name(elements_ID["screen"], elname)
         print("screen name", elname.value.decode())
-        chain_element_by_id(elements_ID["EXP1"], elements_ID["screen"])
+        chain_element_by_id(elements_ID["f"], elements_ID["screen"])
     if test_spot_diagram:
+        assert test_add_element
         set_recording(elements_ID["screen"], RecordingMode.recording_output)
 
     if test_radiate:
@@ -129,18 +132,31 @@ if __name__ == "__main__":
             nrays2 = Parameter()
             get_parameter(sourceID, "nRays", nrays2)
             print("New nrays", nrays2.value)
+
+        # new_distance = Parameter()
+        # get_parameter(elements_ID["pupille"], "distance", new_distance)
+        # new_distance.value = DOUBLE(20-0.9)
+        # set_parameter(elements_ID["pupille"], "distance", new_distance)
+        # get_parameter(elements_ID["pupille"], "distance", new_distance)
+        # print("New distance", new_distance.value)
         diagram = Diagram(ndim=5, nreserved=int(nrays2.value))
-        get_spot_diagram(elements_ID["screen"], diagram, distance=0)
+        get_spot_diagram(elements_ID["screen"], diagram, distance=20)
         print(np.ctypeslib.as_array(diagram.min, shape=(diagram.dim,)))
         print(diagram.count)
         import pandas as pd
         spots = pd.DataFrame(np.ctypeslib.as_array(diagram.spots, shape=(diagram.reserved, diagram.dim)),
-                             columns=("X", "Y", "Z", "dX", "dY"))
+                             columns=("X", "Y", "dX", "dY", "Lambda"))
         # import matplotlib.pyplot as plt
         # plt.scatter(spots["X"], spots["Y"])
         # plt.show()
-        fig = scatter_plot_2d(spots["X"], spots["Y"])
-        show(fig)
+        print(spots.head())
+        figs = []
+        figs.append(plot_spd(spots, x_key="X", y_key="Y", light_plot=True, show_map=True))
+        figs.append(plot_spd(spots, x_key="X", y_key="dX", light_plot=False))
+        figs.append(plot_spd(spots, x_key="Y", y_key="dY", light_plot=True))
+
+        for fig in figs:
+            show(fig)
 
     if test_ID:
         print("ran test_ID")
