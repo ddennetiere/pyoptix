5a694dafb940a0974a8a931adf364884778041a0
David DENNETIERE <david.dennetiere@synchrotron-soleil.fr>
2021-08-06 11:31:25 +0200

    feat: load auto de optix à l'import de pyoptix

--- __init__.py
+++ __init__.py
@@ -1,1 +1,17 @@
 # coding: utf-8
+# import sys
+# sys.path.append("..")
+from .exposed_functions import load_optix
+from bokeh.io import output_notebook as bok_on
+
+global optix
+try:
+    test = optix
+    print(test, "already initialized")
+except NameError:
+    optix = load_optix()
+    print("OptiX library initialized")
+
+
+def output_notebook():
+    bok_on()
